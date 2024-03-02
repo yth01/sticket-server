b0181a0b248d6b380d2252cb7d09309cde79c41d
yth01 <yth011026@gmail.com>
2024-03-03 01:11:04 +0900

    Create base project for sticket-server

---
+++ src/test/java/com/slci/sticket/SticketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slci.sticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
